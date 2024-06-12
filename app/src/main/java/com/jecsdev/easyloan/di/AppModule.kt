8fa5c6b54cdcaa1830e1b9b5913c456c9e763024
john0 <john024996@gmail.com>
2024-06-11 22:32:53 -0400

    Add Firestore dependency

--- app/src/main/java/com/jecsdev/easyloan/di/AppModule.kt
+++ app/src/main/java/com/jecsdev/easyloan/di/AppModule.kt
@@ -1,6 +1,7 @@
 package com.jecsdev.easyloan.di
 
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.jecsdev.easyloan.data.repository.AuthRepository
 import com.jecsdev.easyloan.data.repository.AuthRepositoryImplementation
 import dagger.Module
@@ -19,6 +20,10 @@ object AppModule {
     @Provides
     @Singleton
     fun providesFirebaseAuth() = FirebaseAuth.getInstance()
+
+    @Provides
+    @Singleton
+    fun providesFirebaseFirestore() = FirebaseFirestore.getInstance()
 
     @Provides
     @Singleton
