a125fe6e12b98155b5bdca107ac092e6a59ac174
john0 <john024996@gmail.com>
2024-07-21 17:29:48 -0400

    fix delete animation error

--- app/src/main/java/com/jecsdev/easyloan/ui/composables/swipetodismiss/CustomSwipeToDismissBox.kt
+++ app/src/main/java/com/jecsdev/easyloan/ui/composables/swipetodismiss/CustomSwipeToDismissBox.kt
@@ -1,8 +1,15 @@
 package com.jecsdev.easyloan.ui.composables.swipetodismiss
 
 import android.content.Context
+import androidx.compose.animation.AnimatedVisibility
+
 import androidx.compose.animation.animateColorAsState
+
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.background
+
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -11,22 +18,31 @@ import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.SwipeToDismissBox
+
+
 import androidx.compose.material3.SwipeToDismissBoxValue
 import androidx.compose.material3.rememberSwipeToDismissBoxState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+
+
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 
 import androidx.compose.ui.res.stringResource
+
 import androidx.compose.ui.unit.dp
 import com.jecsdev.easyloan.R
 import com.jecsdev.easyloan.ui.composables.dialog.MessageDialog
+import kotlinx.coroutines.delay
+
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -35,37 +51,57 @@ fun CustomSwipeToDismissBox(
     context: Context,
     onEdit: () -> Unit,
     onDelete: () -> Unit,
-    content: @Composable() () -> Unit
+    animationDuration: Int = 500,
+    content: @Composable () -> Unit
 ) {
-    var showDialog by remember { mutableStateOf(false) }
+    var showDialog by rememberSaveable { mutableStateOf(false) }
     val dismissState = rememberSwipeToDismissBoxState()
     val coroutineScope = rememberCoroutineScope()
-    SwipeToDismissBox(
-        state = dismissState,
-        backgroundContent = {
-            val color by animateColorAsState(
-                targetValue = when (dismissState.targetValue) {
-                    SwipeToDismissBoxValue.Settled -> Color.Transparent
-                    SwipeToDismissBoxValue.EndToStart -> Color.Red
-                    SwipeToDismissBoxValue.StartToEnd -> Color.Transparent
-                }, label = stringResource(id = R.string.empty_string)
-            )
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(color)
-                    .padding(horizontal = 20.dp),
-                contentAlignment = Alignment.CenterEnd
-            ) {
-                Icon(
-                    imageVector = Icons.Default.Delete,
-                    contentDescription = "Delete",
-                    tint = Color.White
-                )
-            }
+    var isRemoved by remember {
+        mutableStateOf(false)
+    }
+
+
+    LaunchedEffect(key1 = isRemoved) {
+        if(isRemoved) {
+            showDialog = false
+            delay(animationDuration.toLong())
         }
+    }
+    AnimatedVisibility(
+        visible = !isRemoved,
+        exit = shrinkVertically(
+            animationSpec = tween(durationMillis = animationDuration),
+            shrinkTowards = Alignment.Top
+        ) + fadeOut()
     ) {
-        content()
+        SwipeToDismissBox(
+            state = dismissState,
+            backgroundContent = {
+                val color by animateColorAsState(
+                    targetValue = when (dismissState.targetValue) {
+                        SwipeToDismissBoxValue.Settled -> Color.Transparent
+                        SwipeToDismissBoxValue.EndToStart -> Color.Red
+                        SwipeToDismissBoxValue.StartToEnd -> Color.Transparent
+                    }, label = stringResource(id = R.string.empty_string)
+                )
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(color)
+                        .padding(horizontal = 20.dp),
+                    contentAlignment = Alignment.CenterEnd
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Delete,
+                        contentDescription = "Delete",
+                        tint = Color.White
+                    )
+                }
+            }
+        ) {
+            content()
+        }
     }
 
     if (showDialog) {
@@ -74,30 +110,33 @@ fun CustomSwipeToDismissBox(
             shouldShowConfirmButton = true,
             shouldShowCancelButton = true,
             onConfirmButtonClicked = {
-                showDialog = false
+                isRemoved = true
                 onDelete()
             },
             onCancelButtonClicked = {
                 showDialog = false
                 coroutineScope.launch {
-                    dismissState.snapTo(SwipeToDismissBoxValue.Settled)
+                    dismissState.reset()
                 }
             }
         )
     }
 
+    LaunchedEffect (dismissState.currentValue) {
+        when (dismissState.currentValue) {
+            SwipeToDismissBoxValue.Settled -> {
 
-    when (dismissState.currentValue) {
-        SwipeToDismissBoxValue.StartToEnd -> {
+            }
 
-        }
+            SwipeToDismissBoxValue.StartToEnd -> {
 
-        SwipeToDismissBoxValue.EndToStart -> {
-            showDialog = true
-        }
+            }
 
-        SwipeToDismissBoxValue.Settled -> {
-
+            SwipeToDismissBoxValue.EndToStart -> {
+                showDialog = true
+            }
         }
     }
 }
+
+
