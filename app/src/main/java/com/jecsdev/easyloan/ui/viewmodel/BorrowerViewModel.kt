2cbd77725e08b0310258a82ee333852c66f87c3d
john0 <john024996@gmail.com>
2024-07-30 09:57:56 -0400

    Clean

--- app/src/main/java/com/jecsdev/easyloan/ui/viewmodel/BorrowerViewModel.kt
+++ app/src/main/java/com/jecsdev/easyloan/ui/viewmodel/BorrowerViewModel.kt
@@ -14,7 +14,6 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.launch
-import java.lang.Thread.State
 import javax.inject.Inject
 
 /**
